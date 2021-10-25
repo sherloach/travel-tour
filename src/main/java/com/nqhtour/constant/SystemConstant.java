752dbac27f14f40abb4a1651e7bbc710b4fa546e
3nt3r <hoango1607@gmail.com>
2021-10-25 22:21:28 +0700

    check ignore

--- src/main/java/com/nqhtour/constant/SystemConstant.java
+++
@@ -1,9 +0,0 @@
-package com.nqhtour.constant;
-
-public class SystemConstant {
-	public static final int ACTIVE_STATUS = 1;
-	public static final int INACTIVE_STATUS = 0;
-
-	public static final String MY_EMAIL = "xxx@gmail.com";
-	public static final String MY_PASSWORD = "xxx";
-}
